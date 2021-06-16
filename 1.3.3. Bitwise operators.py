b10de76778ca319672f93c98d8a6effedee6e830
Amisha Sahu <amishasahu328@gmail.com>
2021-06-16 18:26:26 +0530

    Added Bitwise operator

---
+++ 1.3.3. Bitwise operators.py
@@ -0,0 +1,28 @@
+#
+# Examples of Bitwise operators : Bitwise operators acts on bits and performs bit by bit operation.
+#
+
+def main():
+    a = 10
+    b = 4
+
+    # Print bitwise AND operation  
+    print(a & b)
+
+    # Print bitwise OR operation
+    print(a | b)
+
+    # Print bitwise NOT operation 
+    print(~a)
+
+    # print bitwise XOR operation 
+    print(a ^ b)
+
+    # print bitwise right shift operation 
+    print(a >> 2)
+
+    # print bitwise left shift operation 
+    print(a << 2)
+
+if __name__ == "__main__":
+  main()    
