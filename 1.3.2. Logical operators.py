bb8ca46a85ddf0272287b9b712b0701876c9bc7e
Amisha Sahu <amishasahu328@gmail.com>
2021-06-16 18:21:21 +0530

    Added Logical Operators

---
+++ 1.3.2. Logical operators.py
@@ -0,0 +1,19 @@
+# Examples of Logical Operator
+
+
+def main():
+    a = True
+    b = False
+
+    # Print a and b is False
+    print(a and b)
+
+    # Print a or b is True
+    print(a or b)
+
+    # Print not a is False
+    print(not a)
+
+
+if __name__ == "__main__":
+  main()
