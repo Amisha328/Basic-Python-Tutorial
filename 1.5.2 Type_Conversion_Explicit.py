2517a054670019f958fcbc5bcb7863fd9e5a811a
Amisha Sahu <amishasahu328@gmail.com>
2021-06-19 17:55:05 +0530

    Explicit type Conversion Modified

--- 1.5.2 Type_Conversion_Explicit.py
+++ 1.5.2 Type_Conversion_Explicit.py
@@ -29,3 +29,22 @@ e = float(s)
 print ("After converting to float : ", end="")
 print (e)
 
+# using  ord(), hex(), oct()
+
+# initializing integer
+s = '4'
+
+# printing character converting to integer
+c = ord(s)
+print ("After converting character to integer : ",end="")
+print (c)
+
+# printing integer converting to hexadecimal string
+c = hex(56)
+print ("After converting 56 to hexadecimal string : ",end="")
+print (c)
+
+# printing integer converting to octal string
+c = oct(56)
+print ("After converting 56 to octal string : ",end="")
+print (c)
