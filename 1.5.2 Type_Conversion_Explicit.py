498e54401c177588871649b1306e8786c688f1f9
Amisha Sahu <amishasahu328@gmail.com>
2021-06-19 18:00:19 +0530

    Explicit type Conversion Modified

--- 1.5.2 Type_Conversion_Explicit.py
+++ 1.5.2 Type_Conversion_Explicit.py
@@ -14,89 +14,96 @@
 # 11. complex(real,imag) : : This function converts real numbers to complex(real,imag) number.
 # 12. chr(number) : : This function converts number to its corresponding ASCII character.
 
-# Python code to demonstrate Type conversion
-# using int(), float()
 
-# initializing string
-s = "10010"
+def main():
+    
+    # Python code to demonstrate Type conversion
+    # using int(), float()
 
-# printing string converting to int base 2
-c = int(s,2)
-print ("After converting to integer base 2 : ", end="")
-print (c)
+    # initializing string
+    s = "10010"
 
-# printing string converting to float
-e = float(s)
-print ("After converting to float : ", end="")
-print (e)
+    # printing string converting to int base 2
+    c = int(s,2)
+    print ("After converting to integer base 2 : ", end="")
+    print (c)
 
-# using  ord(), hex(), oct()
+    # printing string converting to float
+    e = float(s)
+    print ("After converting to float : ", end="")
+    print (e)
 
-# initializing integer
-s = '4'
+    # using  ord(), hex(), oct()
 
-# printing character converting to integer
-c = ord(s)
-print ("After converting character to integer : ",end="")
-print (c)
+    # initializing integer
+    s = '4'
 
-# printing integer converting to hexadecimal string
-c = hex(56)
-print ("After converting 56 to hexadecimal string : ",end="")
-print (c)
+    # printing character converting to integer
+    c = ord(s)
+    print ("After converting character to integer : ",end="")
+    print (c)
 
-# printing integer converting to octal string
-c = oct(56)
-print ("After converting 56 to octal string : ",end="")
-print (c)
+    # printing integer converting to hexadecimal string
+    c = hex(56)
+    print ("After converting 56 to hexadecimal string : ",end="")
+    print (c)
 
-# using  tuple(), set(), list()
+    # printing integer converting to octal string
+    c = oct(56)
+    print ("After converting 56 to octal string : ",end="")
+    print (c)
 
-# initializing string
-s = 'python'
+    # using  tuple(), set(), list()
 
-# printing string converting to tuple
-c = tuple(s)
-print ("After converting string to tuple : ",end="")
-print (c)
+    # initializing string
+    s = 'python'
 
-# printing string converting to set
-c = set(s)
-print ("After converting string to set : ",end="")
-print (c)
+    # printing string converting to tuple
+    c = tuple(s)
+    print ("After converting string to tuple : ",end="")
+    print (c)
 
-# printing string converting to list
-c = list(s)
-print ("After converting string to list : ",end="")
-print (c) 
+    # printing string converting to set
+    c = set(s)
+    print ("After converting string to set : ",end="")
+    print (c)
 
-# using  dict(), complex(), str()
+    # printing string converting to list
+    c = list(s)
+    print ("After converting string to list : ",end="")
+    print (c) 
 
-# initializing integers
-a = 1
-b = 2
+    # using  dict(), complex(), str()
 
-# initializing tuple
-tup = (('a', 1) ,('f', 2), ('g', 3))
+    # initializing integers
+    a = 1
+    b = 2
 
-# printing integer converting to complex number
-c = complex(1,2)
-print ("After converting integer to complex number : ",end="")
-print (c)
+    # initializing tuple
+    tup = (('a', 1) ,('f', 2), ('g', 3))
 
-# printing integer converting to string
-c = str(a)
-print ("After converting integer to string : ",end="")
-print (c)
+    # printing integer converting to complex number
+    c = complex(1,2)
+    print ("After converting integer to complex number : ",end="")
+    print (c)
 
-# printing tuple converting to expression dictionary
-c = dict(tup)
-print ("After converting tuple to dictionary : ",end="")
-print (c)
+    # printing integer converting to string
+    c = str(a)
+    print ("After converting integer to string : ",end="")
+    print (c)
 
-# Convert ASCII value to characters
-a = chr(76)
-b = chr(77)
+    # printing tuple converting to expression dictionary
+    c = dict(tup)
+    print ("After converting tuple to dictionary : ",end="")
+    print (c)
 
-print(a)
-print(b)
+    # Convert ASCII value to characters
+    a = chr(76)
+    b = chr(77)
+
+    print(a)
+    print(b)
+
+
+if __name__ == "__main__":
+  main() 
