13d1cfbba73cf8486048b71ea00eec5e7ed340b5
Amisha Sahu <amishasahu328@gmail.com>
2021-06-19 17:54:06 +0530

    Explicit type Conversion

---
+++ 1.5.2 Type_Conversion_Explicit.py
@@ -0,0 +1,31 @@
+# In Explicit Type Conversion in Python, the data type is manually changed by the user as per their requirement. 
+# Various form of explicit type conversion are explained below:
+
+# 1. int(a, base): This function converts any data type to integer. 'Base' specifies the base in which string is if the data type is a string.
+# 2. float(): This function is used to convert any data type to a floating-point number.
+# 3. ord() : This function is used to convert a character to integer.
+# 4. hex() : This function is to convert integer to hexadecimal string.
+# 5. oct() : This function is to convert integer to octal string.
+# 6. tuple() : This function is used to convert to a tuple.
+# 7. set() : This function returns the type after converting to set.
+# 8. list() : This function is used to convert any data type to a list type.
+# 9. dict() : This function is used to convert a tuple of order (key,value) into a dictionary.
+# 10. str() : Used to convert integer into a string.
+# 11. complex(real,imag) : : This function converts real numbers to complex(real,imag) number.
+
+# Python code to demonstrate Type conversion
+# using int(), float()
+
+# initializing string
+s = "10010"
+
+# printing string converting to int base 2
+c = int(s,2)
+print ("After converting to integer base 2 : ", end="")
+print (c)
+
+# printing string converting to float
+e = float(s)
+print ("After converting to float : ", end="")
+print (e)
+
