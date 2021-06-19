c0f17378ee159875b6de03ada5b2d18588471009
Amisha Sahu <amishasahu328@gmail.com>
2021-06-19 17:56:56 +0530

    Explicit type Conversion Modified

--- 1.5.2 Type_Conversion_Explicit.py
+++ 1.5.2 Type_Conversion_Explicit.py
@@ -68,3 +68,27 @@ print (c)
 c = list(s)
 print ("After converting string to list : ",end="")
 print (c) 
+
+# using  dict(), complex(), str()
+
+# initializing integers
+a = 1
+b = 2
+
+# initializing tuple
+tup = (('a', 1) ,('f', 2), ('g', 3))
+
+# printing integer converting to complex number
+c = complex(1,2)
+print ("After converting integer to complex number : ",end="")
+print (c)
+
+# printing integer converting to string
+c = str(a)
+print ("After converting integer to string : ",end="")
+print (c)
+
+# printing tuple converting to expression dictionary
+c = dict(tup)
+print ("After converting tuple to dictionary : ",end="")
+print (c)
