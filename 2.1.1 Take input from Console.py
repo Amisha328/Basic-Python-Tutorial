0323a5905b95d6bacf06d7279ea4c17a1a6c5e73
Amisha Sahu <amishasahu328@gmail.com>
2021-06-20 18:08:24 +0530

    take user input from console in python

---
+++ 2.1.1 Take input from Console.py
@@ -0,0 +1,15 @@
+# What is Console in Python? 
+
+# Console (also called Shell) is basically a command line interpreter that takes input from the user.
+# If it is error free then it runs the command and gives required output otherwise shows the error message. 
+
+# To take input from the user we make use of a built-in function input().
+
+# input
+input1 = input()
+
+# output
+print(input1)
+
+
+
