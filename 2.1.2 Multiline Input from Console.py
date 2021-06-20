40d1422b3845ec46a12eeb8c4bd3a33960401114
Amisha Sahu <amishasahu328@gmail.com>
2021-06-20 18:19:29 +0530

    MultiValue input

---
+++ 2.1.2 Multiline Input from Console.py
@@ -0,0 +1,14 @@
+# What is Console in Python? 
+
+# Console (also called Shell) is basically a command line interpreter that takes input from the user.
+# If it is error free then it runs the command and gives required output otherwise shows the error message. 
+
+# To take input from the user we make use of a built-in function input().
+# Input multiple values from user in one line.
+
+def main():
+    
+    # One solution is to use raw_input() two times.
+    x, y = input(),  input()
+
+    
