93ac3da1d34c18368b6f21cbb498fd7457a83a27
Amisha Sahu <amishasahu328@gmail.com>
2021-06-20 18:10:12 +0530

    type casting user input to float

--- 2.1.1 Take input from Console.py
+++ 2.1.1 Take input from Console.py
@@ -22,3 +22,13 @@ num2 = int(input())
 # printing the sum in integer
 print(num1 + num2)
 
+# Typecasting the input to Float
+
+# input
+num1 = float(input())
+num2 = float(input())
+
+# printing the sum in float
+print(num1 + num2)
+
+
