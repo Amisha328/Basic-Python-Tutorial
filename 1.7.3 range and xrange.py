4a9d4fa5ffd510e8ea070b966751066c2496b702
Amisha Sahu <amishasahu328@gmail.com>
2021-06-23 11:59:36 +0530

    new file created

---
+++ 1.7.3 range and xrange.py
@@ -0,0 +1,8 @@
+# range() and xrange() are two functions that could be used to iterate a certain number of times in for loops in Python.
+
+# In Python 3, there is no xrange , but the range function behaves like xrange in Python 2.
+
+# range() - This returns a range object (a type of iterable).
+# xrange() - This function returns the generator object that can be used to display numbers only by looping.
+#            Only particular range is displayed on demand and hence called "lazy evaluation".
+
