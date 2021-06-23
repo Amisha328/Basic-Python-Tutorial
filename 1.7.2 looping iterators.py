ae6615ef560d73d32add2afb95800278a9b5eba1
Amisha Sahu <amishasahu328@gmail.com>
2021-06-23 11:20:35 +0530

    looping extensions

--- 1.7.2 looping iterators.py
+++ 1.7.2 looping iterators.py
@@ -20,4 +20,27 @@ for i in range(len(cars)):
 #using the enumerate() function to get index 
 days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
 for i, d in enumerate(days):
-    print (i, d)       
+    print (i, d)   
+
+# Looping extensions
+# Two iterators for a single looping construct
+
+# a list and dictionary are to be used for each iteration in a single looping block using enumerate function.
+cars = ["Aston", "Audi", "McLaren"]
+accessories = ["GPS kit", "Car repair-tool kit"]
+
+# Single dictionary holds prices of cars and 
+# its accessories.
+# First three items store prices of cars and
+# next two items store prices of accessories.
+prices = {1:"570000$", 2:"68000$", 3:"450000$",
+          4:"8900$", 5:"4500$"}
+
+# Printing prices of cars
+for index, c in enumerate(cars, start=1):
+    print("Car: %s Price: %s"%(c, prices[index]))
+
+# Printing prices of accessories
+for index, a in enumerate(accessories,start=1):
+    print ("Accessory: %s Price: %s"\
+           %(a,prices[index+len(cars)]))          
