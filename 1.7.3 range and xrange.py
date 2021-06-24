fbc7e3c2020c8c10c568741a6c045fd540e808a4
Amisha Sahu <amishasahu328@gmail.com>
2021-06-24 14:16:53 +0530

    Operational usage of range vs xrange

--- 1.7.3 range and xrange.py
+++ 1.7.3 range and xrange.py
@@ -52,3 +52,21 @@ print (sys.getsizeof(x))    # o/p: 40
 # As range() returns the list, all the operations that can be applied on the list can be used on it.
 # As xrange() returns the xrange object, operations associated to list cannot be applied on them.
 
+# Python code to demonstrate range() vs xrange()
+# on  basis of operations usage 
+
+# initializing a with range()
+a = range(1,6)
+
+# initializing a with xrange()
+x = xrange(1,6)      # not defined in python 3
+
+# testing usage of slice operation on range()
+# prints without error
+print ("The list after slicing using range is : ")
+print (a[2:5])       # o/p: [3, 4, 5]
+
+# testing usage of slice operation on xrange()
+# raises error
+print ("The list after slicing using xrange is : ")
+print (x[2:5])      # o/p: TypeError: sequence index must be integer, not 'slice'
