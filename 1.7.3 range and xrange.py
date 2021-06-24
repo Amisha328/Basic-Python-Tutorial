c2c5a2536ccaed24168902ce066346dfb1dcc831
Amisha Sahu <amishasahu328@gmail.com>
2021-06-24 14:05:45 +0530

    Return type of range vs xrange

--- 1.7.3 range and xrange.py
+++ 1.7.3 range and xrange.py
@@ -6,3 +6,20 @@
 # xrange() - This function returns the generator object that can be used to display numbers only by looping.
 #            Only particular range is displayed on demand and hence called "lazy evaluation".
 
+# Python code to demonstrate range() vs xrange()
+# on  basis of return type
+
+# initializing a with range()
+a = range(1,10000)
+
+# initializing a with xrange()
+x = xrange(1,10000)   # not defined in python 3
+
+# testing the type of a
+print ("The return type of range() is : ")
+print (type(a))       # o/p: <type 'list'> 
+                        
+
+# testing the type of x
+print ("The return type of xrange() is : ")
+print (type(x))      # o/p: <type 'xrange'> 
