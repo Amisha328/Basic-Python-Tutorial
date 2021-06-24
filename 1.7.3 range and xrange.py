2f3f0820795d9740828f19aa0c3017f13903add4
Amisha Sahu <amishasahu328@gmail.com>
2021-06-24 14:11:08 +0530

    Memory of range vs xrange

--- 1.7.3 range and xrange.py
+++ 1.7.3 range and xrange.py
@@ -27,3 +27,23 @@ print (type(x))      # o/p: <type 'xrange'>
 # The variable storing the range created by range() takes more memory as compared to variable storing the range using xrange(). 
 # The basic reason for this is the return type of range() is list and xrange() is xrange() object.
 
+# Python code to demonstrate range() vs xrange()
+# on  basis of memory
+
+import sys
+
+# initializing a with range()
+a = range(1,10000)
+
+# initializing a with xrange()
+x = xrange(1,10000)      # not defined in python 3
+
+# testing the size of a
+# range() takes more memory
+print ("The size allotted using range() is : ")
+print (sys.getsizeof(a))    # o/p: 80064
+
+# testing the size of a
+# range() takes less memory
+print ("The size allotted using xrange() is : ")
+print (sys.getsizeof(x))    # o/p: 40
