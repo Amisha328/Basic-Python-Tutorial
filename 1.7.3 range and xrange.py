b2e57c4f658a44c198a1f80894eeac55895e4e5d
Amisha Sahu <amishasahu328@gmail.com>
2021-06-24 14:19:44 +0530

    Indenation

--- 1.7.3 range and xrange.py
+++ 1.7.3 range and xrange.py
@@ -11,62 +11,69 @@
 
 import sys
 
-# initializing a with range()
-a = range(1,10000)
+def main():
 
-# initializing a with xrange()
-x = xrange(1,10000)   # not defined in python 3
+    # initializing a with range()
+    a = range(1,10000)
 
-# testing the type of a
-print ("The return type of range() is : ")
-print (type(a))       # o/p: <type 'list'> 
-                        
+    # initializing a with xrange()
+    x = xrange(1,10000)   # not defined in python 3
 
-# testing the type of x
-print ("The return type of xrange() is : ")
-print (type(x))      # o/p: <type 'xrange'> 
+    # testing the type of a
+    print ("The return type of range() is : ")
+    print (type(a))       # o/p: <type 'list'> 
+                            
 
-# The variable storing the range created by range() takes more memory as compared to variable storing the range using xrange(). 
-# The basic reason for this is the return type of range() is list and xrange() is xrange() object.
+    # testing the type of x
+    print ("The return type of xrange() is : ")
+    print (type(x))      # o/p: <type 'xrange'> 
 
-# Python code to demonstrate range() vs xrange()
-# on  basis of memory
+    # The variable storing the range created by range() takes more memory as compared to variable storing the range using xrange(). 
+    # The basic reason for this is the return type of range() is list and xrange() is xrange() object.
 
-# initializing a with range()
-a = range(1,10000)
+    # Python code to demonstrate range() vs xrange()
+    # on  basis of memory
 
-# initializing a with xrange()
-x = xrange(1,10000)      # not defined in python 3
+    # initializing a with range()
+    a = range(1,10000)
 
-# testing the size of a
-# range() takes more memory
-print ("The size allotted using range() is : ")
-print (sys.getsizeof(a))    # o/p: 80064
+    # initializing a with xrange()
+    x = xrange(1,10000)      # not defined in python 3
 
-# testing the size of a
-# range() takes less memory
-print ("The size allotted using xrange() is : ")
-print (sys.getsizeof(x))    # o/p: 40
+    # testing the size of a
+    # range() takes more memory
+    print ("The size allotted using range() is : ")
+    print (sys.getsizeof(a))    # o/p: 80064
+
+    # testing the size of a
+    # range() takes less memory
+    print ("The size allotted using xrange() is : ")
+    print (sys.getsizeof(x))    # o/p: 40
 
 
-# As range() returns the list, all the operations that can be applied on the list can be used on it.
-# As xrange() returns the xrange object, operations associated to list cannot be applied on them.
+    # As range() returns the list, all the operations that can be applied on the list can be used on it.
+    # As xrange() returns the xrange object, operations associated to list cannot be applied on them.
 
-# Python code to demonstrate range() vs xrange()
-# on  basis of operations usage 
+    # Python code to demonstrate range() vs xrange()
+    # on  basis of operations usage 
 
-# initializing a with range()
-a = range(1,6)
+    # initializing a with range()
+    a = range(1,6)
 
-# initializing a with xrange()
-x = xrange(1,6)      # not defined in python 3
+    # initializing a with xrange()
+    x = xrange(1,6)      # not defined in python 3
 
-# testing usage of slice operation on range()
-# prints without error
-print ("The list after slicing using range is : ")
-print (a[2:5])       # o/p: [3, 4, 5]
+    # testing usage of slice operation on range()
+    # prints without error
+    print ("The list after slicing using range is : ")
+    print (a[2:5])       # o/p: [3, 4, 5]
 
-# testing usage of slice operation on xrange()
-# raises error
-print ("The list after slicing using xrange is : ")
-print (x[2:5])      # o/p: TypeError: sequence index must be integer, not 'slice'
+    # testing usage of slice operation on xrange()
+    # raises error
+    print ("The list after slicing using xrange is : ")
+    print (x[2:5])      # o/p: TypeError: sequence index must be integer, not 'slice'
+
+if __name__ == "__main__":
+  main()
+
+  
