aba7f5f04a4c2b6cde7da30df3613515b4660615
Amisha Sahu <amishasahu328@gmail.com>
2021-06-27 12:14:25 +0530

    using raw input method

---
+++ 2.2 Take Multiple Input from console.py
@@ -0,0 +1,10 @@
+# What is Console in Python? 
+
+# Console (also called Shell) is basically a command line interpreter that takes input from the user.
+# If it is error free then it runs the command and gives required output otherwise shows the error message. 
+
+
+# use raw_input() two times
+x, y = input(), input()
+
+print(x,y)
