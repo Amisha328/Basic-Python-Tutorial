2ce70d3d2248ec6cdcb0a88b63e20bdf574eaae2
Amisha Sahu <amishasahu328@gmail.com>
2021-07-09 16:07:35 +0530

    yield vs return

--- 2.3 yield vs return.py
+++ 2.3 yield vs return.py
@@ -12,4 +12,28 @@ def simpleGeneratorFun():
 for value in simpleGeneratorFun(): 
     print(value)
 
+# Return sends a specified value back to its caller whereas Yield can produce a sequence of values. 
+# We should use yield when we want to iterate over a sequence, but don't want to store the entire sequence in memory.   
 
+# Yield are used in Python generators. 
+# A generator function is defined like a normal function, but whenever it needs to generate a value, it does so with the yield keyword rather than return.
+
+# A Python program to generate squares from 1 to 100 using yield and therefore generator.
+
+# An infinite generator function that prints
+# next square number.
+def nextSquare():
+    i = 1;
+
+    # An Infinite loop to generate squares 
+    while True:
+        yield i*i                
+        i += 1  # Next execution resumes 
+                # from this point     
+
+# Driver code to test above generator 
+# function
+for num in nextSquare():
+    if num > 100:
+         break    
+    print(num)
