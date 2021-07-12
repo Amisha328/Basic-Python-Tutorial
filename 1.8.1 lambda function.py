1d27250b15ca441a635f4e13f2a72098d452a773
Amisha Sahu <amishasahu328@gmail.com>
2021-07-12 12:52:05 +0530

    lambda function with map()

--- 1.8.1 lambda function.py
+++ 1.8.1 lambda function.py
@@ -32,3 +32,10 @@ li = [5, 7, 22, 97, 54, 62, 77, 23, 73, 61]
 final_list = list(map(lambda x: x*2, li))
 print(final_list)
 
+animals = ['dog', 'cat', 'parrot', 'rabbit']
+
+# here we intend to change all animal names
+# to upper case and return the same
+uppered_animals = list(map(lambda animal: str.upper(animal), animals))
+
+print(uppered_animals)
