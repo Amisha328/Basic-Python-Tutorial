d3c0873f1ba8a3c38042abf7503f15f3ec20a652
Amisha Sahu <amishasahu328@gmail.com>
2021-07-12 12:50:42 +0530

    lambda function with map()

--- 1.8.1 lambda function.py
+++ 1.8.1 lambda function.py
@@ -22,3 +22,13 @@ li = [5, 7, 22, 97, 54, 62, 77, 23, 73, 61]
 
 final_list = list(filter(lambda x: (x%2 != 0) , li))
 print(final_list)
+
+# Using lambda() Function with map()
+# The map() function takes in a function and a list as an argument.
+# map() with lambda()
+# to get double of a list.
+li = [5, 7, 22, 97, 54, 62, 77, 23, 73, 61]
+
+final_list = list(map(lambda x: x*2, li))
+print(final_list)
+
