c3f8d5c777e3c4b3facedb64a82ab2b047d00b92
Amisha Sahu <amishasahu328@gmail.com>
2021-07-12 12:38:26 +0530

    lambda function with filter()

--- 1.8.1 lambda function.py
+++ 1.8.1 lambda function.py
@@ -14,3 +14,11 @@ print(cube(5))
 # using the lambda function
 print(lambda_cube(5))
 
+# Using lambda() Function with filter()
+# filter() offers elegant way to filter out all the elements of a sequence "sequence", for which the function returns True. 
+
+# filter() with lambda()
+li = [5, 7, 22, 97, 54, 62, 77, 23, 73, 61]
+
+final_list = list(filter(lambda x: (x%2 != 0) , li))
+print(final_list)
