2776ac70d2fc809e68938f908938ec22a6ff9d46
Amisha Sahu <amishasahu328@gmail.com>
2021-07-12 11:22:34 +0530

    empty function

--- 1.8. functions.py
+++ 1.8. functions.py
@@ -3,26 +3,22 @@
 #
 
 # define a basic function
-
-
 def func1():
     print("I am a function")
 
+# define empty functions, we use pass statement
+def fun(): 
+    pass
+
 # function that takes arguments
-
-
 def func2(arg1, arg2):
     print(arg1, " ", arg2)
 
 # function that returns a value
-
-
 def cube(x):
     return x*x*x
 
 # function with default value for an argument
-
-
 def power(num, x=1):
     result = 1
     for i in range(x):
@@ -30,8 +26,6 @@ def power(num, x=1):
     return result
 
 # function with variable number of arguments
-
-
 def multi_add(*args):
     result = 0
     for x in args:
