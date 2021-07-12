5351ae880636814c4eb252070412a70b74ee33ce
Amisha Sahu <amishasahu328@gmail.com>
2021-07-12 12:12:30 +0530

    added math class functions

--- 2.0.0 modules.py
+++ 2.0.0 modules.py
@@ -7,3 +7,20 @@ print("The square root of 16 is", math.sqrt(16))
 # in addition to functions, some modules contain useful constants 
 print("Pi is:", math.pi)
 
+# 2 radians = 114.59 degreees
+print(math.degrees(2))  
+
+# 60 degrees = 1.04 radians
+print(math.radians(60))  
+
+# Sine of 2 radians
+print(math.sin(2))  
+
+# Cosine of 0.5 radians
+print(math.cos(0.5))  
+
+# Tangent of 0.23 radians
+print(math.tan(0.23)) 
+
+# 1 * 2 * 3 * 4 = 24
+print(math.factorial(4))  
