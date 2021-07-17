6398b42ae1102163376b7c867130202be7289118
Amisha Sahu <amishasahu328@gmail.com>
2021-07-17 13:19:23 +0530

    grabage collection

---
+++ 2.5 garbage collection.py
@@ -0,0 +1,16 @@
+# Python's memory allocation and deallocation method is automatic.
+
+# The literal value 9 is an object.
+# The reference count of object 9 is incremented to 1 in line 1.
+# In line 2 its reference count becomes zero as it is dereferenced.
+# So garbage collector deallocates the object.
+
+# Literal 9 is an object
+b = 9
+
+# Reference count of object 9 
+# becomes 0.
+b = 4
+
+
+
