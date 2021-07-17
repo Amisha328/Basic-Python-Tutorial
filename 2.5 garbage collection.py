2c18da5eeb87fccc15d7b7a8b7e492c9d0044ea9
Amisha Sahu <amishasahu328@gmail.com>
2021-07-17 13:20:17 +0530

    grabage collection by reference cycle

--- 2.5 garbage collection.py
+++ 2.5 garbage collection.py
@@ -12,5 +12,18 @@ b = 9
 # becomes 0.
 b = 4
 
+# create_cycle() creates an object x which refers to itself, 
+# the object x will not automatically be freed when the function returns. 
+def create_cycle():
+
+    # create a list x
+    x = [ ]
+
+    # A reference cycle is created
+    # here as x contains reference to
+    # to self.
+    x.append(x)
+ 
+create_cycle()
 
 
